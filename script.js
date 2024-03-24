f8167256d86047647e70babb0d9bc321fa3c0ef4
grzegorz.ziolek <104593451+grzegorz-ziolek@users.noreply.github.com>
2024-03-24 10:42:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ function play(cell) {
         cell.textContent = currentPlayer;
         if (gameMode === 'single' && currentPlayer === 'O') {
             setTimeout(computerMove, 500); // Komputer wykonuje ruch po 0.5 sekundy
-        } else {
+            // Nie zmieniaj tutaj currentPlayer, ponieważ zostanie on zmieniony po ruchu komputera w funkcji computerMove
+        } else if (gameMode === 'multi') {
             currentPlayer = currentPlayer === 'O' ? 'X' : 'O';
         }
     }
@@ -21,10 +22,12 @@ function computerMove() {
     const availableCells = Array.from(document.querySelectorAll('.cell')).filter(c => c.textContent === '');
     if (availableCells.length > 0) {
         const randomCell = availableCells[Math.floor(Math.random() * availableCells.length)];
-        randomCell.textContent = currentPlayer;
-        currentPlayer = 'O';
+        randomCell.textContent = 'X'; // Tutaj zmieniamy wartość na 'X'
+        // Po ruchu komputera, następnym graczem będzie znowu użytkownik, więc jeśli gra jest w trybie jednoosobowym, użytkownik zawsze będzie miał 'O'
+        if (gameMode === 'single') currentPlayer = 'O';
     }
 }
+
 
 function resetGame() {
     document.querySelectorAll('.cell').forEach(cell => {
